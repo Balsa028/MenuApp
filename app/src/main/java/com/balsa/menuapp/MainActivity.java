23d51dd68ce8b70d1fb23607367deab8015225b4
Balsa Pavlicevic <balsa.pavlicevic@teletrader.com>
2022-09-03 18:52:28 +0200

    Network connection loss handled on all screens.

--- app/src/main/java/com/balsa/menuapp/MainActivity.java
+++ app/src/main/java/com/balsa/menuapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.balsa.menuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.balsa.menuapp.Login.LoginFragment;
+import com.balsa.menuapp.Utils.NetworkConnectivity;
 import com.balsa.menuapp.Utils.Util;
 import com.balsa.menuapp.Venues.VenuesListFragment;
 
@@ -13,11 +18,14 @@ import com.balsa.menuapp.Venues.VenuesListFragment;
 public class MainActivity extends AppCompatActivity {
 
     private String token;
+    private NetworkConnectivity networkConnectivity;
+    private TextView connectivityLost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleConnectivityChanges();
 
        // Util.saveTokenInSharedPrefs("", this); //---> za logout, dok se ne napravi logout onClick
         token = Util.readTokenFromSharedPrefs(this);
@@ -31,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    private void handleConnectivityChanges() {
+        connectivityLost = findViewById(R.id.txtConnectivityLost);
+        networkConnectivity = new NetworkConnectivity(this);
+        networkConnectivity.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isOnline) {
+                if(!isOnline){
+                    connectivityLost.setVisibility(View.VISIBLE);
+                } else connectivityLost.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
     private void decideStartScreen() {
         if(token.equals("")){
             Util.replaceFragment(getSupportFragmentManager(), R.id.fragment_container, LoginFragment.newInstance());
---
+++ app/src/main/java/com/balsa/menuapp/Utils/NetworkConnectivity.java
@@ -0,0 +1,71 @@
+package com.balsa.menuapp.Utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+public class NetworkConnectivity extends LiveData<Boolean> {
+
+    ConnectivityManager connectivityManager;
+
+    //constructor
+    public NetworkConnectivity(Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            connectivityManager = context.getSystemService(ConnectivityManager.class);
+        }
+    }
+    //creating network request object
+    NetworkRequest networkRequest = new NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build();
+
+    //creating network callback
+    private ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback(){
+
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+            postValue(true);
+        }
+
+        @Override
+        public void onLost(@NonNull Network network) {
+            super.onLost(network);
+            postValue(false);
+        }
+
+        @Override
+        public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+            super.onCapabilitiesChanged(network, networkCapabilities);
+            boolean isInternetWorking = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+            boolean isInternetValidated = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+            postValue(isInternetWorking && isInternetValidated);
+        }
+    };
+
+    //kada postoji makar jedan aktivan observer
+    @Override
+    protected void onActive() {
+        super.onActive();
+        //ukoliko je aktivan neki observer registrujemo network callback
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback);
+    }
+
+    //kada nema aktivnih observera
+    @Override
+    protected void onInactive() {
+        super.onInactive();
+        //ukoliko nema aktivnih observera odregistrujemo network callback
+        connectivityManager.unregisterNetworkCallback(networkCallback);
+    }
+
+
+}
